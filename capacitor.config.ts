ffdbac5fa4b6217d2d2cf77c5f6f1a54b0ea0eb9
rgudipudibeadpay <rgudipudi@beadpay.io>
2024-06-06 19:06:01 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'background-ionic',
+  webDir: 'www'
+};
+
+export default config;
